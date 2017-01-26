eb8d63ef7283c1776272d8073747525223f7b22a
staticfox <staticfox@staticfox.net>
2017-01-26 03:03:10 -0500

    chef: Skeleton chef file

---
+++ src/chef.c
@@ -0,0 +1,32 @@
+/*
+ *   chef.c -- Ladle - Chef Bootstrapper
+ *   Copyright 2017 - Matt Ullman <staticfox@staticfox.net>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void
+setup_directories(const char *directory)
+{
+    struct stat st = {0};
+
+    if (stat("./directory", &st) == -1)
+        mkdir("./directory", 0700);
+
+}
